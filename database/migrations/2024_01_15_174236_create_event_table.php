f50118d636332353afd51d2d92c4396311688440
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-01-15 19:01:16 +0100

    events table migrated

---
+++ database/migrations/2024_01_15_174236_create_event_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('date');
+            $table->string('location');
+            $table->text('description');
+            $table->text('image');
+            $table->text('author');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event');
+    }
+};
