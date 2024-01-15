1b6538a966fc38aae4ce227da9245c51f9db359b
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-01-15 20:32:14 +0100

    Exercise c done

--- database/migrations/2024_01_15_174236_create_event_table.php
+++ database/migrations/2024_01_15_174236_create_event_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->text('description');
             $table->text('image');
             $table->text('author');
+            $table->timestamps();
 
         });
     }
